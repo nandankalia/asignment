5ad0fa514f64ead9f8a01b39e2ecba224a605bcb
nandankalia <99944363+nandankalia@users.noreply.github.com>
2022-07-03 14:05:20 +0530

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,40 @@
+# # 16. Write a Python program to get the difference between a given number and 17, if the number is greater than 17 return double the absolute difference.
+
+# # a = int(input("enter a number: "))
+# # if a < 17:
+# #     print("difference is: ",17-a)
+# # else:
+# #     b = a-17
+# #     c = abs(b)
+# #     print("given number ia greater than 17 hence double of its absoulte difference is " , 2*c)
+
+# #17. Write a Python program to test whether a number is within 100 of 1000 or 2000. 
+
+# # a = int(input("enter the number : "))
+# # if (a >= 900 and a <= 1100):
+# #     print("given number is with in 100 of 1000")
+# # elif( a >= 1900 and a <=2100):
+# #     print ("given is with 100 of 2000 ")
+# # else:
+# #     print("given is not with in 100 of 1000  or 2000")    
+
+# 18. Write a Python program to calculate the sum of three given numbers, if the values are equal then return three times of their sum
+
+# a = float(input("enter a number :"))
+# b = float(input("enter a number :"))
+# c = float(input("enter a number :"))
+
+# if (a==b and b==c):
+#     d = a+b+c
+#     print(d*3)
+# else:
+#     print (a+b+c)
+
+#19. Write a Python program to get a new string from a given string where "Is" has been added to the front. If the given string already begins with "Is" then return the string unchanged.
+
+# string = str(input("Enter a string : "))
+# if (len(string) >=2 and string[:2] == 'Is'):
+#     print(string)
+# else:
+#     print("Is"+string)
+
