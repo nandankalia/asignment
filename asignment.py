e89ad51da70031757129acc31d257f38d2beb6f5
nandankalia <99944363+nandankalia@users.noreply.github.com>
2022-07-07 06:47:55 +0530

    Add files via upload

---
+++ asignment.py
@@ -0,0 +1,30 @@
+# 20. Write a Python program to get a string which is n (non-negative integer) copies of a given string.
+
+# a = str(input("enter your striing :"))
+# b = int(input("enter your rep: "))
+
+# ans = ""
+# for i in range (b):
+#     ans = ans + a
+
+# print(ans)
+
+#21 Write a Python program to find whether a given number (accept from the user) is even or odd, print out an appropriate message to the user. 
+
+# a = float(input("enter a number :"))
+# s = a % 2
+# if s > 0:
+#     print("this  is an odd number")
+# else:
+#     print("this  is an even number")
+
+# 22. Write a Python program to count the number 4 in a given list. 
+
+# a = [1,2,1,4,4,4,6,7,8,9,0]
+# count={}
+# for i in a:
+#     if i in count:
+#         count[i]+=1
+#     else:
+#         count[i]=1
+# print(count)        
